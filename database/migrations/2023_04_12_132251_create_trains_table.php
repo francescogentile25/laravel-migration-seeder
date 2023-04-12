5000d8384487e0c4788f1f5a8e06b3e641c17230
francescogentile25 <francescogentile250893@gmail.com>
2023-04-12 16:27:41 +0200

    ERROR

---
+++ database/migrations/2023_04_12_132251_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference', 10);
+            $table->string('agency', 200);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->unsignedSmallInteger('train_number');
+            $table->tinyInteger('number_carriages')->unsigned();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
