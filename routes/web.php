41f4ed63f4c6fc2da8efd78820c01966defb0fc4
francescogentile25 <francescogentile250893@gmail.com>
2023-04-12 17:04:37 +0200

    FEAT add db call

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'homepage']);
