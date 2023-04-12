158731db9e0f997e9105873a6802fdb82fb4444e
francescogentile25 <francescogentile250893@gmail.com>
2023-04-12 17:29:57 +0200

    FEAT add train card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,15 +18,25 @@
 <body>
 
     <main>
-        @forelse ($trains as $train)
-            <ul>
-                <li>
-                    {{ $train->agency }}
-                </li>
-            </ul>
+        <div class="container">
+            <h1>Treni in partenza</h1>
+            @forelse ($trains as $train)
+                <ul class="list-group w-50
+                my-4">
+                    <li class="list-group-item">{{ $train->agency }}</li>
+                    <li class="list-group-item"> Da: {{ $train->departure_station }}</li>
+                    <li class="list-group-item">A: {{ $train->arrival_station }}</li>
+                    <li class="list-group-item"> Orario partenza: {{ $train->departure_time }}</li>
+                    <li class="list-group-item">Orario arrivo: {{ $train->arrival_time }}</li>
+                    <li class="list-group-item">Numero Treno: {{ $train->train_number }}</li>
+                    <li class="list-group-item">Numero Carrozze: {{ $train->number_carriages }}</li>
+                    <li class="list-group-item"> In orario: {{ $train->in_time ? 'si' : 'no' }}</li>
+                    <li class="list-group-item"> Cancellato: {{ $train->deleted ? 'si' : 'no' }}</li>
+                </ul>
 
-        @empty
-        @endforelse
+            @empty
+            @endforelse
+        </div>
     </main>
 
 </body>
