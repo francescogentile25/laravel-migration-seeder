b8d7c2c8d3d01269fe78884130f8f5140e5815a1
francescogentile25 <francescogentile250893@gmail.com>
2023-04-13 17:17:20 +0200

    FEAT add db train info

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Train;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,22 +14,22 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Train $faker)
+    public function run(Faker $faker)
     {
         $agency = ['Italo', 'Trenitalia', 'TreNord', 'FerrovieGatgano', 'Hogwarts'];
         for ($i = 0; $i < 100; $i++) {
             $new_train = new Train();
             $new_train->reference = $faker->bothify('??-######');
-            $new_train->reference = $faker->randomElement($agency);
-            $new_train->reference = $faker->city();
-            $new_train->reference = $faker->city();
-            $new_train->reference = $faker->time();
-            $new_train->reference = $faker->time();
-            $new_train->reference = $faker->randomNumber(1, 99);
-            $new_train->reference = $faker->randomNumber(1, 12);
-            $new_train->reference = $faker->randomElement([0, 1]);
-            $new_train->reference = $faker->randomElement([0, 1]);
-            $new_train->reference = $faker->randomFloat(2, 1, 999);
+            $new_train->agency = $faker->randomElement($agency);
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_number = $faker->randomNumber(1, 99);
+            $new_train->number_carriages = $faker->randomNumber(1, 12);
+            $new_train->in_time = $faker->randomElement([0, 1]);
+            $new_train->deleted = $faker->randomElement([0, 1]);
+            $new_train->price = $faker->randomFloat(2, 1, 999);
             $new_train->save();
         }
     }
